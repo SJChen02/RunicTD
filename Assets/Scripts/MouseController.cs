da95714f9caba514fe83efd95a8aecbc80c29db7
Matty <matslyk0@gmail.com>
2024-10-19 20:36:05 +0100

    Clean Up after Merge
    Cleaned up after a messy merge. Couple things still need to be corrected:
    - Gold feature works but text not rendering properly
    - Waves need to be remade

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -4,26 +4,11 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour {
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-<<<<<<< HEAD
     public GameObject towerPrefab;
-=======
-    public GameObject prefab;
->>>>>>> dev
-=======
-    public GameObject towerPrefab;
->>>>>>> Stashed changes
-=======
-    public GameObject prefab;
->>>>>>> dev
     private Vector3 offsetVector = new Vector3(0, 5, 0);
 
     private void OnMouseDown() {
         if (CompareTag("Tile")) {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-<<<<<<< HEAD
             placeTower(towerPrefab);
         }
     }
@@ -40,39 +25,7 @@ public class MouseController : MonoBehaviour {
             FortressGold.gold -= 10;
         }
 
-=======
-=======
->>>>>>> dev
-            Destroy(gameObject);
-            Instantiate(
-                prefab, 
-                gameObject.transform.position + offsetVector, 
-                Quaternion.identity, 
-                GameObject.Find("Dummy Towers").transform
-            );
-<<<<<<< HEAD
-=======
-            placeTower(towerPrefab);
->>>>>>> Stashed changes
+
         }
->>>>>>> dev
+
     }
-
-    private void placeTower(GameObject towerType) {
-        if (FortressGold.gold >= 10) {
-            Destroy(gameObject);
-            Instantiate(
-                towerPrefab,
-                gameObject.transform.position + offsetVector,
-                Quaternion.identity,
-                GameObject.Find("Towers").transform
-            );
-            FortressGold.gold -= 10;
-        }
-
-=======
-        }
->>>>>>> dev
-    }
-
-}
