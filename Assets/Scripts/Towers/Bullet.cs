8445a6544b688eeccec4926c9680b2b0b5f147ac
rAise-IV <82789550+rAise-IV@users.noreply.github.com>
2024-12-14 13:28:50 +0000

    Bullet class converted to abstract class

--- Assets/Scripts/Towers/Bullet.cs
+++ Assets/Scripts/Towers/Bullet.cs
@@ -2,59 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public abstract class Bullet : MonoBehaviour
 {
-    private Transform target;
-
+    protected Transform target;
     public float speed = 70f;
-
     public int damage = 40;
 
     public void Seek(Transform _target) {
-
         target = _target;
-
     }
-
-
-
 
     // Update is called once per frame
     void Update() {
-
         if (target == null) {
-
             Destroy(gameObject);
             return;
-
         }
 
         Vector3 dir = target.position - transform.position;
         float distanceThisFrame = speed * Time.deltaTime;
 
         if (dir.magnitude <= distanceThisFrame) { //dir.magnitude is distance to target
-
             HitTarget();
             return;
-
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
-
     }
 
-    void HitTarget() {
-
-        Enemy enemy = target.GetComponent<Enemy>();
-
-        if (enemy != null) {
-
-            enemy.TakeDamage(damage);
-
-        }
-
-        Destroy(gameObject);
-
-    }
-
+    protected abstract void HitTarget();
 }
