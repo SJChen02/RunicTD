cca792066ac770adbfd5a4ec97d667e66b56cd8a
Parsa Rafiee <120268324+Parsa-Rafiee@users.noreply.github.com>
2025-01-05 13:57:23 +0000

    fixes

---
+++ Assets/Scripts/In-game Menus/Sell.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sell : MonoBehaviour {
+
+    public static void SellTower() {
+
+        // adding the value of the tower to the mana pool
+        Tower towerScript = Controller.currentTower.GetComponent<Tower>();
+        Fortress.mana += towerScript.sellValue;
+
+        // accessing the tile the tower is placed on and reactivating it
+        Tile tileScript = towerScript.tilePlacedOn.GetComponent<Tile>();
+        tileScript.towerPlaced = false;
+        Controller.currentTile = null;
+
+        // close menu and clear runes
+        Controller.ToggleRunicTablet();
+
+        // deleting the tower
+        Destroy(Controller.currentTower);
+
+        // close the menu
+        Controller.CloseTowerMenu();
+    }
+}
---
+++ Assets/Scripts/In-game Menus/Store.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Store : MonoBehaviour {
+
+    private Vector3 offsetVector = new Vector3(0, 5, 0);
+
+    public void PurchaseTower() {
+
+        // referencing the script to access the tower cost in the if statement
+        GameObject towerSelected = TowerIdentifier.GetTower();
+        Tower towerScript = towerSelected.GetComponent<Tower>();
+
+        if (Fortress.mana >= towerScript.cost) {
+
+            // flagging the tile as deactivated
+            Tile tileScript = Controller.currentTile.GetComponent<Tile>();
+            tileScript.towerPlaced = true;
+
+            // saving which tile the tower is placed on, for selling
+            towerScript.tilePlacedOn = Controller.currentTile;
+
+            // unhighlighting the tile
+            Controller.currentTile.GetComponent<Renderer>().material = Controller.tileMaterial;
+
+            // placing the tower
+            Instantiate(
+                towerSelected,
+                Controller.currentTile.transform.position + offsetVector,
+                Quaternion.identity,
+                GameObject.Find("Towers").transform
+            );
+
+            // paying for the tower, unselecting the tile, closing the store
+            Fortress.mana -= towerScript.cost;
+            Controller.currentTile = null;
+            Controller.store.SetActive(false);
+        }
+    }
+}
