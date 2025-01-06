cb51b3ebd89c4caac333c9511644a0ad43ad3772
Parsa Rafiee <120268324+Parsa-Rafiee@users.noreply.github.com>
2025-01-06 22:11:34 +0000

    threading error fixed
    fixed the threading error that caused the build to stop functioning.

--- Assets/Scripts/Waves/Spawner.cs
+++ Assets/Scripts/Waves/Spawner.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class Spawner : MonoBehaviour {
+public class Spawner : MonoBehaviour
+{
     [SerializeField] private GameObject spawnPoint;
     [SerializeField] private PathManager pathManager;
     public Wave[] waves;
     public float enemySpawnCooldown = 1f;
+    private bool isSpawning = false;
 
-    private void Start() {
+    private void Start()
+    {
         // Calculate the total number of enemies in the waves
         foreach (Wave wave in waves)
         {
@@ -18,46 +21,52 @@ public class Spawner : MonoBehaviour {
         makeReport();
     }
 
-    // makeReport() is called by WaveTracker when the wave is over to track each Spawners enemies left
-    public void makeReport() {
-        WaveTracker.ReportEnemiesLeft(waves[WaveTracker.currentWave].enemiesLeft);
-    }
-
-    /* SpawnWave() is a coroutine that spawns enemies in the scene. It is called by WaveTracker when the wave is over.
-     * It spawns enemies in the scene with a delay of enemySpawnCooldown between each enemy.
-     * It also registers the spawned enemies in the WaveTracker. (used in tower targetting)
-     */
-    public IEnumerator SpawnWave() { // IEnumerator is a type of function that can be paused and resumed 
+    public void makeReport()
+    {
         if (WaveTracker.currentWave < waves.Length)
         {
-            foreach (var enemySet in waves[WaveTracker.currentWave].enemySets)
+            // Just report the count for the next wave, don't add to totalEnemiesLeft yet
+            WaveTracker.PrepareNextWave(waves[WaveTracker.currentWave].enemiesLeft);
+        }
+    }
+
+    public IEnumerator SpawnWave()
+    {
+        // Check if we're already spawning or if we're past the last wave
+        if (isSpawning || WaveTracker.currentWave >= waves.Length)
+        {
+            yield break;
+        }
+
+        isSpawning = true;
+        Wave currentWave = waves[WaveTracker.currentWave];
+
+        foreach (var enemySet in currentWave.enemySets)
+        {
+            for (int j = 0; j < enemySet.count; j++)
             {
-                for (int j = 0; j < enemySet.count; j++)
-                {
-                    Enemy spawnedEnemy = Instantiate(enemySet.prefab, spawnPoint.transform);
-                    spawnedEnemy.transform.SetParent(this.transform);
-                    spawnedEnemy.SetPathManager(pathManager);
-                    WaveTracker.RegisterEnemy(spawnedEnemy);
-                    yield return new WaitForSeconds(enemySpawnCooldown);
-                }
+                Enemy spawnedEnemy = Instantiate(enemySet.prefab, spawnPoint.transform);
+                spawnedEnemy.transform.SetParent(this.transform);
+                spawnedEnemy.SetPathManager(pathManager);
+                WaveTracker.RegisterEnemy(spawnedEnemy);
+                yield return new WaitForSeconds(enemySpawnCooldown);
             }
         }
 
+        isSpawning = false;
+        currentWave.enemiesLeft = 0; // Reset enemies left for this wave
     }
 }
 
-/* Wave is a class that holds the details of the enemies that will be spawned in a wave.*/
-[System.Serializable] // This attribute allows us to see the class in the inspector
-public class Wave {
+[System.Serializable]
+public class Wave
+{
     [System.Serializable]
     public struct EnemyDetail
     {
         public Enemy prefab;
         public int count;
     }
-
     public List<EnemyDetail> enemySets;
-
-
-    [HideInInspector] public int enemiesLeft; // "HideInInspector" hides the variable in the inspector
+    [HideInInspector] public int enemiesLeft;
 }
